8ab06ff29879ee76eeddbbbe5b2488e1efcf4ebb
Audrey Apple <appleaudrey1@gmail.com>
2025-04-27 19:31:59 -0500

    created the first class for the maze tiles and move the png into correct dir

--- src/project.py
+++ src/project.py
@@ -2,6 +2,21 @@
 import pygame
 
 
+
+
+# class tho defin the the maze tiles and what they do (wall, passage, finish)
+# class for making the maze
+# class for the player movement and animation of the player 
+# add to main function for it to work
+
+
+class Tile:
+    def __init__(self, x, y, thinkness):
+        self.x, self.y = x, y
+        self.thinkness =thinkness
+        self.walls ={'top':True, 'right': True, 'bottom':True, 'left':True}
+        self.visited =False
+
 def main():
     pygame.init()
     pygame.display.set_caption("Hungry Bunny!!")
@@ -18,9 +33,11 @@ def main():
                 running = False
 
 
-        green = (124, 197, 118)
+        green = pygame.Color(124, 197, 118)
         screen.fill(green)
-        pygame.display.flip
+
+
+        pygame.display.flip()
         
         
         if  pygame.key.get_pressed()[pygame.K_ESCAPE]:
