800a043dbb0be4c68700de2e0809a0c650dacdbd
Audrey Apple <appleaudrey1@gmail.com>
2025-05-01 09:58:49 -0500

    change the resolution to have hight and width, color to yellow and comments

--- src/project.py
+++ src/project.py
@@ -1,44 +1,73 @@
 # import random
 import pygame
+import sys
 
 
 
-
-# class tho defin the the maze tiles and what they do (wall, passage, finish)
+# class to defin the the maze tiles and what they do (wall, passage, finish)
 # class for making the maze
 # class for the player movement and animation of the player 
-# add to main function for it to work
 
 
-class Tile:
-    def __init__(self, x, y, thinkness):
-        self.x, self.y = x, y
-        self.thinkness =thinkness
-        self.walls ={'top':True, 'right': True, 'bottom':True, 'left':True}
-        self.visited =False
+
+# class Maze:
+    
+#     def __init__(self):
+
+#        self.tile_maze = [[1,1,0,1,0,0,0,0,0,1,1,1,0,0],
+#                         [0,1,0,1,1,1,1,0,0,0,0,1,0,0],
+#                         [0,1,0,0,0,0,1,0,1,0,0,1,0,0],
+#                         [1,1,1,1,0,0,1,0,1,0,1,1,0,0],
+#                         [0,0,0,1,0,0,1,1,1,0,1,0,0,0],
+#                         [0,0,0,1,0,0,1,0,1,0,1,1,1,1],
+#                         [0,1,1,1,1,0,1,0,1,0,1,0,0,1],
+#                         [0,1,0,0,0,0,1,0,1,1,1,0,1,1],
+#                         [0,1,0,1,1,1,1,0,0,1,0,0,0,0],
+#                         [1,1,0,1,0,0,1,0,1,1,1,1,1,1],
+#                         [1,1,1,1,0,1,1,0,1,1,1,0,0,2]]
+
+       
+    
+        
+
+
+    # def draw():
+        # refernce to the tile method using 0 and 1 
+
+
+    # def player():
+        # need a way to check tiles to determine if the player can go there or not
+
+        # input key result in movement of the player + animation of player 
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
     pygame.display.set_caption("Hungry Bunny!!")
     
+    height = 900
+    width = 700
     
+    screen = pygame.display.set_mode((height,width), pygame.RESIZABLE)
 
-    resolution =(900,700)
-    screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
+    
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                sys.exit()
 
+          
 
-        green = pygame.Color(124, 197, 118)
-        screen.fill(green)
+        yellow = pygame.Color(255, 250, 179)
+        screen.fill(yellow)
 
 
         pygame.display.flip()
-        
+        clock.tick(24)
         
         if  pygame.key.get_pressed()[pygame.K_ESCAPE]:
             pygame.QUIT()
